8b0f218caef6b3dbab40d59dd0b8c979c946c1a3
Nixon1018 <nixon.joshwa@tringapps.com>
2023-01-30 16:12:31 +0530

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,31 +30,34 @@ class Student
         LOGGER.log(Level.INFO,Double.toString((gpa)),"the gpa is:"+(gpa));
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String studentname;
         String gradelevel;
         int gpa;
         int choice;
-        Scanner sc= new Scanner (System.in);
-        LOGGER.log(Level.INFO,"Enter the Studentname:");
-        studentname=sc.next();
-        LOGGER.log(Level.INFO,"Enter the gradelevel");
-        gradelevel=sc.next();
-        LOGGER.log(Level.INFO,"Enter the gpa:");
-        gpa=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        try{
+        LOGGER.log(Level.INFO, "Enter the Studentname:");
+        studentname = sc.next();
+        LOGGER.log(Level.INFO, "Enter the gradelevel");
+        gradelevel = sc.next();
+        LOGGER.log(Level.INFO, "Enter the gpa:");
+        gpa = sc.nextInt();
         Student s = new Student(studentname, gradelevel, gpa);
-        LOGGER.log(Level.INFO,"Enter the choice:");
-        LOGGER.log(Level.INFO,"1.update 2.display");
-        choice=sc.nextInt();
-        if(choice==1)
-        {
+        LOGGER.log(Level.INFO, "Enter the choice:");
+        LOGGER.log(Level.INFO, "1.update 2.display");
+        choice = sc.nextInt();
+        if (choice == 1) {
             gpa = s.update();
-            LOGGER.log(Level.INFO,Double.toString((gpa)),"The updated student gpa is:"+(gpa));
-        }
-        else if(choice==2)
-        {
+            LOGGER.log(Level.INFO, Double.toString((gpa)), "The updated student gpa is:" + (gpa));
+        } else if (choice == 2) {
             s.display();
+        }
+    }catch (Exception e)
+        {
+            String ec = ""+e;
+            LOGGER.info(ec);
+            sc.nextLine();
         }
     }
 }
